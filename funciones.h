3b4e9da721ddf189f2212bd4908cc11454d4fd19
PaulaAndradeC <maria.andrade.cuesta@udla.edu.ec>
2025-01-26 18:19:21 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,43 +1,29 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-#define NUM_ZONAS 3
+#define NUM_ZONAS 5
 #define NUM_CONTAMINANTES 4
-#define DIAS_HISTORICOS 7
+#define NUM_DIAS_HISTORICOS 30 
 
 typedef struct {
-    float niveles[NUM_CONTAMINANTES];
+    float niveles_contaminantes[NUM_CONTAMINANTES];
     float temperatura;
     float velocidad_viento;
     float humedad;
-} Datos;
-
-typedef struct {
-    float niveles[NUM_CONTAMINANTES];
-} Prediccion;
-
-typedef struct {
-    float niveles[NUM_CONTAMINANTES];
-} Historico;
-
-typedef struct {
-    char nombre[50];
-    Datos actual;
-    Prediccion prediccion;
-    Historico historico[DIAS_HISTORICOS];
-    float promedios_historicos[NUM_CONTAMINANTES];
+    float historial_contaminacion[NUM_DIAS_HISTORICOS][NUM_CONTAMINANTES];  
 } Zona;
 
-float promedio_ponderado(const float *valores, int n, int *error);
-int guardar_datos(const Zona *zonas, const char *nombre_archivo);
-int cargar_datos(Zona *zonas, const char *nombre_archivo);
-void simular_datos_historicos(Zona *zonas);
-int ingresar_datos_actuales(Zona *zonas);
-void mostrar_predicciones(const Zona *zonas);
-void calcular_promedios_historicos(Zona *zonas);
-void mostrar_comparacion_oms(const Zona *zonas);
-void calcular_predicciones(Zona *zonas);
-void generar_alertas_y_recomendaciones(const Zona *zonas);
-void ejecutar_menu(Zona *zonas);
+
+void ingresar_datos_actuales(Zona zonas[NUM_ZONAS]);
+void calcular_niveles_contaminacion_actual(Zona zonas[NUM_ZONAS]);
+void calcular_predicciones(Zona zonas[NUM_ZONAS]);
+void mostrar_comparacion_oms(Zona zonas[NUM_ZONAS]);
+void generar_alertas_y_recomendaciones(Zona zonas[NUM_ZONAS]);
+void calcular_promedios_historicos(Zona zonas[NUM_ZONAS]);
+void exportar_datos(Zona zonas[NUM_ZONAS]);
+
+extern const char *NOMBRE_CONTAMINANTES[NUM_CONTAMINANTES];
+extern const float LIMITES_OMS[NUM_CONTAMINANTES];
 
 #endif
+
