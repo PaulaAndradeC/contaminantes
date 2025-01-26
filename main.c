b5c9c816dd2c779b91a6b655f8035411f5e9ac38
PaulaAndradeC <maria.andrade.cuesta@udla.edu.ec>
2025-01-26 18:19:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,40 @@
 #include "funciones.h"
 
 int main() {
-    Zona zonas[NUM_ZONAS];
-    cargar_datos(zonas, "datos.dat");
+    Zona zonas[NUM_ZONAS] = {0}; // Inicializar todas las zonas
 
-    ejecutar_menu(zonas);
+    int opcion;
+    do {
+        printf("\nMenu de Opciones:\n");
+        printf("1. Ingresar datos actuales\n");
+        printf("2. Calcular niveles actuales de contaminacion\n");
+        printf("3. Mostrar comparacion con limites OMS\n");
+        printf("4. Generar alertas y recomendaciones\n");
+        printf("5. Salir\n");
+        printf("Ingrese una opcion: ");
+        scanf("%d", &opcion);
 
-    guardar_datos(zonas, "datos.dat");
+        switch (opcion) {
+            case 1:
+                ingresar_datos_actuales(zonas);
+                break;
+            case 2:
+                calcular_niveles_contaminacion_actual(zonas);
+                break;
+            case 3:
+                mostrar_comparacion_oms(zonas);
+                break;
+            case 4:
+                generar_alertas_y_recomendaciones(zonas);
+                break;
+            case 5:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opcion no valida. Intente de nuevo.\n");
+                break;
+        }
+    } while (opcion != 5);
 
     return 0;
 }
